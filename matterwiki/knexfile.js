ab3f559c1089c89c1e09a37be9d52e9d957b2fc8
Hydrog3n <loic.vaille@gmail.com>
2018-02-08 22:19:38 +0100

    Add matterwiki docker

---
+++ matterwiki/knexfile.js
@@ -0,0 +1,11 @@
+module.exports = {
+    client: 'mysql',
+    connection: {
+      host : process.env.MYSQL_HOST,
+      user : process.env.MYSQL_USER,
+      password : process.env.MYSQL_PASSWORD,
+      database : process.env.MYSQL_DATABASE
+    },
+    useNullAsDefault: true
+  }
+
